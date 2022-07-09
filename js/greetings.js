2c6c7103650bd7b6f623f60621b8d9619d0d1ecd
jolim97 <physicsjong1@gmail.com>
2022-07-10 03:43:42 +0900

    Initial commit

---
+++ js/greetings.js
@@ -0,0 +1,39 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+// Constant string variable with upper case.
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    console.log(username);
+    if(username != ""){
+        localStorage.setItem(USERNAME_KEY,username);
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        paintGreetings(username);
+    }
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+    alert("click");
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
