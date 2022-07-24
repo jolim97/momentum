c38189ee486678789520051a9dd28f9a6ce3e694
jolim97 <physicsjong1@gmail.com>
2022-07-25 01:36:04 +0900

    Rename background for fixing typo

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const imgs = ["picture1.jpeg", "picture2.jpeg", "picture3.jpeg","picture5.jpeg", "picture6.jpeg", "picture7.jpeg",];
+
+const chosenImg = imgs[Math.floor(Math.random() * imgs.length)];
+
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${chosenImg}`;
+
+bgImg.className = 'background-img';
+document.body.appendChild(bgImg);
+
+const bgImgObj = document.querySelector('.background-img').clientHeight;
