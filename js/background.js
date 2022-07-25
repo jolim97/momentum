b1de49b5ef5f424dc5b21db799b32667d5869b63
jolim97 <physicsjong1@gmail.com>
2022-07-26 02:41:24 +0900

    Updates

--- js/background.js
+++ js/background.js
@@ -4,6 +4,12 @@ const chosenImg = imgs[Math.floor(Math.random() * imgs.length)];
 
 const bgImg = document.createElement("img");
 bgImg.src = `img/${chosenImg}`;
+const bgImgPath = `../img/${chosenImg}`
+// document.body.style.background = "url(" + bgImgPath + ")";
+// document.body.style.backgroundSize = 'contain';
+// document.body.style.backgroundAttachment = 'fixed';
+// document.body.style.backgroundRepeat = 'no-repeat';
+// console.log(document.body.style.background);
 
 bgImg.className = 'background-img';
 document.body.appendChild(bgImg);
